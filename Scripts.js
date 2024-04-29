094129bfca66a0b38b132bd1c3e0a5e45cd14197
ARYANNJ <aryanjadhav71@gmail.com>
2024-04-30 01:07:40 +0530

    added API ENDPOINT

--- Scripts.js
+++ Scripts.js
@@ -1,5 +1,6 @@
 // Add your API endpoint here
-var API_ENDPOINT = "API_ENDPOIND_PASTE_HERE";
+var API_ENDPOINT =
+  "https://jqwttd38qh.execute-api.us-east-1.amazonaws.com/prod";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function () {
